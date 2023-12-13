dd18f1c1c524c1bd79f7477fc3f0f884248a6329
gtz.jesus <gtz.jesus@outlook.com>
2023-12-12 17:07:45 -0700

    Company committed

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,7 +1,7 @@
 import Carousel from '../../features/carousel/Carousel';
-import Company from '../../features/company/company';
 import VideoComponent from '../../features/video/VideoComponent';
 import Vital from '../../features/vital/Vital';
+import Company from '../../features/company/Company';
 
 function Landing() {
   return (
