9878e1bdad37eceb1692243bb4e6c756e19ca291
gtz.jesus <gtz.jesus@outlook.com>
2023-12-29 09:59:35 -0700

    WorldHello Background figma design

--- src/features/contact/Contact.jsx
+++ src/features/contact/Contact.jsx
@@ -1,26 +1,40 @@
 import styled from 'styled-components';
+import Button from '../../ui/buttons/Button';
 
 const StyledContact = styled.div`
   display: flex;
-  height: 100vh;
+  height: var(--height-full-window);
   background-image: url('/backgrounds/contact.png');
   background-size: cover;
   color: var(--color-white);
+  padding: var(--padding-medium);
 `;
 
 const Information = styled.div`
   display: flex;
-  margin: 0 auto;
+  flex-direction: column;
+  gap: var(--gap-medium);
 `;
 
 const Title = styled.span`
   font-size: var(--font-large);
+`;
+
+const Description = styled.span`
+  font-size: var(--font-xsmall);
 `;
 function Contact() {
   return (
     <StyledContact>
       <Information>
         <Title>Get in touch</Title>
+        <Description>
+          If you have a general inquiry and would like to speak with our team,
+          you can contact us via email at: contact@worldhello.us
+        </Description>
+        <Button variation="secondary" size="small">
+          Call us for free
+        </Button>
       </Information>
     </StyledContact>
   );
