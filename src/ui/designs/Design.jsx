257244fc1a56839b5a8ee0d47f1a17052f1dd935
gtz.jesus <gtz.jesus@outlook.com>
2024-01-01 16:41:02 -0700

    Changes in the UI carousel

--- src/ui/designs/Design.jsx
+++ src/ui/designs/Design.jsx
@@ -3,7 +3,18 @@ import styled from 'styled-components';
 const StyledDesign = styled.div`
   display: inline-block;
   overflow: hidden;
-  max-width: 100%;
+  height: 50vh;
+`;
+
+const Video = styled.video`
+  left: 50%;
+  top: 50%;
+  min-width: 100%;
+  min-height: 100%;
+  -webkit-transform: translate(-50%, -50%);
+  -moz-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
 `;
 
 function Design({ design }) {
@@ -24,9 +35,9 @@ function Design({ design }) {
 
   return (
     <StyledDesign onClick={handleNavigation}>
-      <video autoPlay="autoPlay" loop muted playsInline={true}>
+      <Video preload="auto" autoPlay="autoPlay" loop muted playsInline={true}>
         <source src={finalDesign.video} type="video/mp4" />
-      </video>
+      </Video>
     </StyledDesign>
   );
 }
