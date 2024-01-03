726322194351fddeed8f3af70e8ffa0d48467955
gtz.jesus <gtz.jesus@outlook.com>
2024-01-03 15:35:56 -0700

    Changing navigation

--- src/ui/navigation/NavigationWBackground.jsx
+++ src/ui/navigation/NavigationWBackground.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const StyledNav = styled.nav`
   position: fixed;
-  background: var(--color-black);
+  background: var(--color-green);
   color: var(--color-white);
   top: 0;
   width: 100vw;
