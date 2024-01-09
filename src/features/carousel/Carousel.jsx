696470fe28428abf212080d4a09b10d0b0ef64ec
gtz.jesus <gtz.jesus@outlook.com>
2024-01-09 15:26:03 -0700

    Created video for ecommerce SPA + HANDBRAKE video file reducer

--- src/features/carousel/Carousel.jsx
+++ src/features/carousel/Carousel.jsx
@@ -160,7 +160,7 @@ const ImageCaption = styled.div`
 
 function Carousel() {
   // DELAY NEEDED FOR CAROUSEL setTIMEOUT
-  const delay = 17500;
+  const delay = 20500;
   // use useState TO KEEP TRACK OF DESIGN + SLIDESHOW
   const [index, setIndex] = useState(0);
   // NEED TO CLEAR OUR TIME IF click on buttons
