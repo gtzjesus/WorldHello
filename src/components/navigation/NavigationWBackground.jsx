c49bf90f15413a87ca2e1e24f318ec2377f60e1d
gtz.jesus <gtz.jesus@outlook.com>
2024-01-18 14:37:35 -0700

    Navigation + logo done!

--- src/components/navigation/NavigationWBackground.jsx
+++ src/components/navigation/NavigationWBackground.jsx
@@ -1,69 +1,88 @@
+// File: Navigation.js
+// Description: React navigation Component (header).
+// LazyLoading from React for optimization (images & web app loading time).
+// Global Styles from /src/styles/ used for global variables.
+// useNavigate react hook to navigate react router (links)
+
+// ------------------------------
+// Imports
+// ------------------------------
+// This section has all necessary imports for this component.
+
 import styled from 'styled-components';
 import { useNavigate } from 'react-router';
+import LazyLoad from 'react-lazyload';
+
+// ------------------------------
+// Styled Componenets
+// ------------------------------
+// This section has all CSS styles configured for every HTML element.
 
 const StyledNav = styled.nav`
-  position: fixed;
-  background: var(--color-black);
-  color: var(--color-white);
-  top: 0;
-  width: 100vw;
-  z-index: var(--z-top);
-  height: var(--height-navigation);
-
-  // FONTS
-  font-size: var(--font-xsmall);
-  // ARRANGE CHILDREN
+  // Code logic to arrange children
   display: flex;
   align-items: center;
   justify-content: space-between;
-
-  @media (min-width: 61.25em) {
-    font-size: var(--font-small);
-  }
+  // Code logic for positioning fixed in all web app + header config
+  position: fixed;
+  background: var(--color-black);
+  color: var(--color-white);
+  width: var(--width-full-window);
+  z-index: var(--z-top);
+  height: var(--height-navigation);
+  font-size: var(--font-xxxsmall);
 `;
 
-const Brand = styled.span`
-  // ARRANGE CHILDREN
+const Brand = styled.div`
+  // Code logic to arrange children
   display: flex;
   align-items: center;
   padding: 0 0 0 var(--padding-small);
   gap: var(--gap-small);
 
+  // @MEDIAQUERY logic for bigger devices (responsive design)
   @media (min-width: 61.25em) {
-    padding: 0 0 0 var(--padding-medium);
-    gap: var(--gap-medium);
   }
 `;
 
 const Logo = styled.img`
-  height: 2.5rem;
+  // Code logic to modify logo
+  height: var(--logo-height);
 `;
 
-const Title = styled.span``;
+const Title = styled.span`
+  // TODO: Code logic to modify brand title
+`;
 
-const Menu = styled.div`
+const Menu = styled.a`
+  // Code logic for contact link a element
   padding: 0 var(--padding-small) 0 0;
-
-  @media (min-width: 61.25em) {
-    padding: 0 var(--padding-medium) 0 0;
-  }
 `;
-function NavigationWBackground() {
-  const navigate = useNavigate();
 
+// ------------------------------
+// Component
+// ------------------------------
+// This section has our React Component which handles the hook data
+
+function NavigationWBackground() {
+  // Grab react hook to navigate the router
+  const navigate = useNavigate();
+  // Handles the navigation to contact page
   function handleContact() {
     navigate('/contact');
   }
-
+  // Handles the navigation to home page
   function handleHome() {
     navigate('/');
   }
   return (
     <StyledNav>
+      {/* <!-- Brand Container for logo and title --> */}
       <Brand onClick={handleHome}>
         <Logo src="/logos/helloworld.png" alt="globe"></Logo>
         <Title>WorldHello</Title>
       </Brand>
+      {/* <!-- Menu Container for links --> */}
       <Menu onClick={handleContact}>Contact</Menu>
     </StyledNav>
   );
